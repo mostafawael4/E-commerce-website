a6d32c9cac67730e976523c4846ac04c0e6ef767
mostafa wael <mw038818@gmail.com>
2024-09-05 15:07:16 +0300

    solve some problem

--- src/app/authenticationpages/authenticationpages.component.ts
+++ src/app/authenticationpages/authenticationpages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../layout/addition/footer/footer.component";
 
 @Component({
   selector: 'app-authenticationpages',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './authenticationpages.component.html',
   styleUrl: './authenticationpages.component.scss'
 })
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../layout/addition/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../layout/addition/footer/footer.component";
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [NavbarComponent , RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
